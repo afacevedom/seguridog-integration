972cb5f2bc3d6e6d58726e48b65a67d59636935f
Mauricio Mejía Castro <jemmejiaca@unal.edu.co>
2015-10-12 11:15:35 -0500

    Se crea la clase de dominio K9User

---
+++ grails-app/domain/co/com/seguridog/K9User.groovy
@@ -0,0 +1,34 @@
+package co.com.seguridog
+
+import java.time.LocalDate
+import java.time.Month
+
+class K9User {
+    Integer cedula
+    String lastName
+    String firstName
+    String loginUser
+    String loginPass
+    LocalDate birthDate
+    String cellphone
+    String eMail
+
+    int getAge() {
+        birthDate.compareTo(LocalDate.now())
+    }
+
+    String toString() {
+        firstName + " " + lastName
+    }
+
+    static constraints = {
+        cedula unique: true
+        lastName maxSize: 40
+        firstName maxSize: 40
+        birthDate min: LocalDate.of(1900, Month.JANUARY, 1)
+        cellphone size: 10
+        eMail email: true
+        loginUser unique: true
+        loginPass()
+    }
+}
