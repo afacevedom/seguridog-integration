3aed5ab09603dab819a879b6e0640e83ac78d9f8
Edwin Alexander <eabohorquezg@unal.edu.co>
2015-10-23 21:32:45 -0500

    Se especifican los constraints en la clase User

--- grails-app/domain/co/com/seguridog/K9User.groovy
+++ grails-app/domain/co/com/seguridog/K9User.groovy
@@ -1,34 +1,30 @@
 package co.com.seguridog
 
-import java.time.LocalDate
-import java.time.Month
-
 class K9User {
+
     Integer cedula
-    String lastName
     String firstName
+    String lastName
     String loginUser
     String loginPass
-    LocalDate birthDate
     String cellphone
     String eMail
-
-    int getAge() {
-        birthDate.compareTo(LocalDate.now())
-    }
 
     String toString() {
         firstName + " " + lastName
     }
 
     static constraints = {
-        cedula unique: true
-        lastName maxSize: 40
-        firstName maxSize: 40
-        birthDate min: LocalDate.of(1900, Month.JANUARY, 1)
-        cellphone size: 10..11
-        eMail email: true
-        loginUser unique: true
-        loginPass()
+        cedula nullable: false, blank: false, unique: true
+        lastName nullable: false, blank: false, maxSize: 40
+        firstName nullable: false, blank: false, maxSize: 40
+        cellphone nullable: false, blank: false, size: 7..10
+        eMail nullable: false, blank: false, email: true
+        loginUser nullable: false, blank: false, unique: true
+        loginPass(nullable: false, blank: false, unique:true, length:8..15, validator:{val, obj ->
+            if(val?.equalsIgnoreCase(obj.firstName)) {
+                return false
+            }
+        })
     }
 }
